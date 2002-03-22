d8ee47e16f9f0d397d27549dba93e899071fd8f9
burnett <>
2002-03-22 15:39:07 +0000

    update to proper version of xml.

--- cmt/setup.bat
+++ cmt/setup.bat
@@ -1,7 +1,7 @@
 @echo off
 if .%1==. (set tag=Win32Debug ) else set tag=%1
 set tempfile=%HOMEDRIVE%%HOMEPATH%tmpsetup.bat
-c:\glast\CMT\v1r10p20011126\VisualC\cmt.exe -quiet -bat -pack=g4dev -version=v0 setup -tag=%tag% >"%tempfile%"
+c:\glast\CMT\v1r10p20011126\VisualC\cmt.exe -quiet -bat -pack=Gleam -version=v0r0p2 setup -tag=%tag% >"%tempfile%"
 if exist "%tempfile%" call "%tempfile%"
 if exist "%tempfile%" del "%tempfile%"
 set PATH=%LD_LIBRARY_PATH%;%PATH%
