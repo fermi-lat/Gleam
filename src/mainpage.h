60d9d291cb7564df105626e2d2b9686c39254719
burnett <>
2002-05-31 02:03:54 +0000

    should have had mainpage.h before

---
+++ src/mainpage.h
@@ -0,0 +1,17 @@
+/** @mainpage package Gleam
+* @author Toby Burnett
+*
+* @section intro Introduction
+* This package defines the Gleam application
+*
+* <hr>
+* @section  jobOptions jobOptions
+*
+*
+* <hr>
+* @section notes release notes
+* release.notes
+* @section requirements requirements
+* @include requirements
+*
+*/
