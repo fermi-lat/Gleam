67c881632affc4b6cac14eb8c5b6e39b8b159a26
als <>
2002-11-02 01:51:31 +0000

    translation of Karl's systest scripts and macros to windows environment

---
+++ src/systests/VerticalGamma100MeV/systest_VerticalGamma100MeV.bat
@@ -0,0 +1,6 @@
+set SYSTEST=VerticalGamma100MeV
+call ..\..\..\cmt\setup.bat
+%GLEAMROOT%\Win32Debug\Gleam.exe %GLEAMROOT%\src\systests\%SYSTEST%\jobOptions.txt
+%ROOTSYS%\bin\root -b -q RUN_Windows
+move Histograms.root %SYSTEST%Histos.root
+del mc.root digi.root recon.root
