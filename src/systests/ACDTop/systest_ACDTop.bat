d897f439c4fdff2bee6e5c159b8603d4aa81517d
heather <>
2002-12-17 22:26:39 +0000

    update test scripts

--- src/systests/ACDTop/systest_ACDTop.bat
+++ src/systests/ACDTop/systest_ACDTop.bat
@@ -1,6 +1,4 @@
-set SYSTEST=AcdTop
+set SYSTEST=ACDTop
 call ..\..\..\cmt\setup.bat
-%GLEAMROOT%\Win32Debug\Gleam.exe %GLEAMROOT%\src\systests\%SYSTEST%\jobOptions.txt
-%ROOTSYS%\bin\root -b -q RUN_Windows
-move Histograms.root %SYSTEST%Histos.root
-del mc.root digi.root recon.root
+call %GLEAMROOT%\src\systests\%SYSTEST%\test_Mip.bat
+
